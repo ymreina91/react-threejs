b47693a7ca13725a5f591b989023e7b21e643eb9
Yuriseidis M. Reina Torres <ymreina91@gmail.com>
2024-03-29 13:33:55 -0500

    fix: rotation prop format

--- src/components/ModelRender.tsx
+++ src/components/ModelRender.tsx
@@ -1,8 +1,7 @@
-// ModelRender.tsx
-import React, { useRef } from 'react';
+import React, { useRef, useMemo } from 'react';
 import { useLoader } from '@react-three/fiber';
 import { STLLoader } from 'three/examples/jsm/Addons.js';
-import { Mesh } from 'three';
+import { Mesh, Euler } from 'three';
 import { TransformControls } from '@react-three/drei';
 import {ModelRenderProps} from "../types";
 
@@ -14,10 +13,15 @@ const ModelRender: React.FC<ModelRenderProps> = (props: ModelRenderProps) => {
     
     const meshRef = useRef<Mesh>(null);
 
+    const rotationEuler = useMemo(() => {
+        const [x, y, z] = initialRotation || [0, 0, 0];
+        return new Euler(x, y, z, 'XYZ'); // Asegúrate de que las rotaciones están en radianes
+    }, [initialRotation]);
+
     return (
       <>
         <TransformControls mode="translate">
-            <mesh ref={meshRef} scale={initialScale} position={initialPosition} rotation={initialRotation}>
+            <mesh ref={meshRef} scale={initialScale} position={initialPosition} rotation={rotationEuler}>
                 <primitive object={geom} attach="geometry" />
                 <meshStandardMaterial color={color} />
             </mesh>
